7d59d2e3a39792fb5cd45ef2f7842cca9f40dccd
Agosh Saini <agosh.saini@gmail.com>
2019-09-12 22:45:16 -0400

    updated data files to original

---
+++ data_cleanup/data_clean_up.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sept 12, 2019
+
+@author: as7saini@edu.uwaterloo.ca
+
+dataset from: 
+https://www.kaggle.com/ehallmar/nba-historical-stats-and-betting-data/downloads/nba-historical-stats-and-betting-data.zip/1
+"""
+
+import numpy as np
+import pandas as pd
+
+np.random.seed(8)
