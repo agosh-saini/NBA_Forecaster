c76b779de2eeee69f4561754c75d82c45427be79
Agosh Saini <agosh.saini@gmail.com>
2019-09-13 00:03:14 -0400

    Created two filter functions

---
+++ data_cleanup/filter_money_line_games_S2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 23:36:37 2019
+
+@author: Agosh Saini (as7saini@edi.uwaterloo.ca)
+"""
+
+#Cleans up data
+#filters only for ____ website odds
+
+import numpy as np
+import pandas as pd
+
+np.random.seed(8)
+
+#creates 
+def clean_up_bets(df, name):
+    df = df[df['book_name']==name]
+    return df
+
+bet_data = pd.read_csv(r'sources/nba_betting_money_line.csv', skipinitialspace=True)
+
+bet_data = clean_up_bets(bet_data, 'Pinnacle Sports')
+
+bet_data.to_csv(r'modified_data/S2_nba_betting_book.csv')
