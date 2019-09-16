fe6d2f2ca8e2684456e0987e246f9e68769f737f
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2019-09-15 21:52:47 -0400

    removed dummy index columns

--- data_cleanup/create_boxscore_and_join_tables_S4.py
+++ data_cleanup/create_boxscore_and_join_tables_S4.py
@@ -4,38 +4,41 @@ Created on Sep 14 2019
 
 @author: Agosh Saini (as7saini@edi.uwaterloo.ca)
 """
-#This file creates boxscore in nba_games
-#Also joins boxscore with betting files
+# This file creates boxscore in nba_games
+# Also joins boxscore with betting files
 
 import numpy as np
 import pandas as pd
 
 np.random.seed(8)
 
+
 def create_boxscore(df):
-    #drops NaN, resets index, and generates boxscore coloumn
+    # drops NaN, resets index, and generates boxscore coloumn
     df = df.dropna()
-    df = df.reset_index().drop('index', axis = 1)
+    df = df.reset_index().drop('index', axis=1)
     df['boxscore'] = ['']*len(df)
-    
+
     for i in range(len(df)-1):
-        #creates boxscore value and adds to df
+        # creates boxscore value and adds to df
         date = df.loc[i, 'game_date']
         date = str(date).replace('-', '')
         team_id = df.loc[i, 'team_id']
         boxscore = date + '0' + team_id
-        
+
         df.loc[i, 'boxscore'] = boxscore
-        
+
     return df
 
+
 def boxscore_merge(df1, df2):
-    #merges boxscore to betting data
+    # merges boxscore to betting data
     df3 = df1.merge(df2, how='inner', on='game_id')
     return df3
 
-team_id_data = pd.read_csv(r'modified_data/S3_team_id_replaced_data.csv', 
-                                     skipinitialspace=True)
+
+team_id_data = pd.read_csv(r'modified_data/S3_team_id_replaced_data.csv',
+                           skipinitialspace=True)
 
 modified_odds_data = pd.read_csv(r'modified_data/S2_nba_betting_book.csv',
                                  skipinitialspace=True)
@@ -43,6 +46,6 @@ modified_odds_data = pd.read_csv(r'modified_data/S2_nba_betting_book.csv',
 boxscore_replaced_data = create_boxscore(team_id_data)
 
 finalized_data_bets = boxscore_merge(modified_odds_data,
-                                     boxscore_replaced_data.loc[:,['game_id','boxscore']])                                  
+                                     boxscore_replaced_data.loc[:, ['game_id', 'boxscore']])
 
-finalized_data_bets.to_csv(r'modified_data/S4_finalized_data.csv')
+finalized_data_bets.to_csv(r'modified_data/S4_finalized_data.csv', index=False)
--- data_cleanup/filter_money_line_games_S2.py
+++ data_cleanup/filter_money_line_games_S2.py
@@ -5,21 +5,25 @@ Created on Thu Sep 12 23:36:37 2019
 @author: Agosh Saini (as7saini@edi.uwaterloo.ca)
 """
 
-#Cleans up data
-#filters only for Pinnacle Sports website odds
+# Cleans up data
+# filters only for Pinnacle Sports website odds
 
 import numpy as np
 import pandas as pd
 
 np.random.seed(8)
 
-#Looks for Pinnacle Sports in the book_name coloumn and sorts out everything else
+# Looks for Pinnacle Sports in the book_name coloumn and sorts out everything else
+
+
 def clean_up_bets(df, name):
-    df = df[df['book_name']==name]
+    df = df[df['book_name'] == name]
     return df
 
-bet_data = pd.read_csv(r'sources/nba_betting_money_line.csv', skipinitialspace=True)
+
+bet_data = pd.read_csv(
+    r'sources/nba_betting_money_line.csv', skipinitialspace=True)
 
 bet_data = clean_up_bets(bet_data, 'Pinnacle Sports')
 
-bet_data.to_csv(r'modified_data/S2_nba_betting_book.csv')
+bet_data.to_csv(r'modified_data/S2_nba_betting_book.csv', index=False)
